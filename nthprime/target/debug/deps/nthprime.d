5a652f383f3d4e9d455714cb13e1427c768d463e
Jonathan Kee <48512757+jonathan-kee@users.noreply.github.com>
2020-07-27 21:40:14 +0800

    Delete nthprime.d

--- nthprime/target/debug/deps/nthprime.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jonat\Desktop\Exercism\nthprime\target\debug\deps\nthprime.exe: src\main.rs
-
-C:\Users\jonat\Desktop\Exercism\nthprime\target\debug\deps\nthprime.d: src\main.rs
-
-src\main.rs:
